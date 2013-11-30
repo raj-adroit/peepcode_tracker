7b2f4f769294c96e6538459fff949b2196607946
Raj Adroit <raja.nice23@gmail.com>
2013-11-30 17:00:55 +0530

    Commit the secret_key need for deployment in heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VideoTracker::Application.config.secret_key_base = '7411452944ab662130aa654774c60c32286d12c6f70be98c6dcf4b4e8015cdd1c4ed24cfc4bad2c70ae8965cbd44e4a5f5fd5d150aa6a0146e0489f866d13c37'
